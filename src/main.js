262d7afd4c273883ff1750a0e139ca1fa9fe4d87
hejy47 <32435376+hejy47@users.noreply.github.com>
2019-05-29 20:12:40 +0800

    登录界面

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+
+Vue.use(ElementUI, { size: 'small', zIndex: 3000 })
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
+axios.defaults.baseURL = '/api'
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  render: h => h(App),
   router,
   components: { App },
   template: '<App/>'
