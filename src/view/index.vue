692026117ce5f3fa62e1aeba5711290d030477d8
hejy47 <32435376+hejy47@users.noreply.github.com>
2019-06-16 19:32:52 +0800

    Update index.vue

--- src/view/index.vue
+++ src/view/index.vue
@@ -1,14 +1,17 @@
 <template>
-  <el-tabs
-    tab-position="left"
-    id="index"
-  >
-    <el-tab-pane label="身份审核">
-      <identity />
-    </el-tab-pane>
-    <el-tab-pane label="任务审核">配置管理</el-tab-pane>
-    <el-tab-pane label="用户管理">角色管理</el-tab-pane>
-  </el-tabs>
+  <div id="container">
+    <h2 id="title">挣闲钱管理系统</h2>
+    <el-tabs
+      tab-position="left"
+      id="index"
+    >
+      <el-tab-pane label="身份审核">
+        <identity />
+      </el-tab-pane>
+      <el-tab-pane label="任务审核">配置管理</el-tab-pane>
+      <el-tab-pane label="用户管理">角色管理</el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
 <script>
@@ -35,7 +38,16 @@ export default {
 </script>
 
 <style scoped>
-#index {
+#container {
   height: 100%;
+}
+#title {
+  height: 6%;
+  margin: 0;
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+  padding-left: 20px;
+}
+#index {
+  height: 94%;
 }
 </style>
