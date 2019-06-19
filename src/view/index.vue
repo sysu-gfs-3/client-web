90148985ce14f0c486b1fdabedee5ead38adb1a3
jiangysh <31833870+534265373@users.noreply.github.com>
2019-06-19 17:59:32 +0800

    先保存一波

--- src/view/index.vue
+++ src/view/index.vue
@@ -1,29 +1,73 @@
 <template>
   <div id="container">
     <h2 id="title">挣闲钱管理系统</h2>
-    <el-tabs
-      tab-position="left"
-      id="index"
-    >
-      <el-tab-pane label="身份审核">
-        <identity />
-      </el-tab-pane>
-      <el-tab-pane label="任务审核">配置管理</el-tab-pane>
-      <el-tab-pane label="用户管理">角色管理</el-tab-pane>
-    </el-tabs>
+    <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+      <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+        <el-radio-button :label="false">展开</el-radio-button>
+        <el-radio-button :label="true">收起</el-radio-button>
+      </el-radio-group>
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-document"></i>
+          <span slot="title">用户管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1">用户审核</el-menu-item><identity></identity>
+          <el-menu-item index="1-2">用户目录</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-menu"></i>
+          <span slot="title">任务管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="2-1">任务审核</el-menu-item>
+          <el-menu-item index="2-2">任务目录</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="3">
+        <template slot="title">
+          <i class="el-icon-setting"></i>
+          <span slot="title">设置</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="3-1">个人信息</el-menu-item>
+          <el-menu-item index="3-2">修改密码</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
   </div>
 </template>
 
 <script>
 import Identity from '../components/identity'
+// export default {
+//   name: 'index',
+//   components: {
+//     identity: Identity
+//   },
+//   data () {
+//     return {
+//       msg: 'Welcome to Your Vue.js App'
+//     }
+//   },
+//   methods: {
+//     handleOpen (key, keyPath) {
+//       console.log(key, keyPath)
+//     },
+//     handleClose (key, keyPath) {
+//       console.log(key, keyPath)
+//     }
+//   }
+// }
 export default {
-  name: 'index',
   components: {
     identity: Identity
   },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      isCollapse: true
     }
   },
   methods: {
@@ -50,4 +94,9 @@ export default {
 #index {
   height: 94%;
 }
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+}
+
 </style>
